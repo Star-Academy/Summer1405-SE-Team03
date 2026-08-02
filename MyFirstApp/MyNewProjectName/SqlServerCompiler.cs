3a3a674176a1b2c028c8cf5738dbc0ebac771e65
Mahdi Kazemaini <mokazemaini84@gmail.com>
2026-08-02 12:18:32 +0330

    Add fluent query implementations for Postgres and SqlServer(all of the files yet

--- MyFirstApp/MyNewProjectName/SqlServerCompiler.cs
+++ MyFirstApp/MyNewProjectName/SqlServerCompiler.cs
@@ -2,30 +2,31 @@
 
 namespace MyNewProjectName;
 
-public class PostgresCompiler
+public class SqlServerCompiler
 {
     public (string sql , string binding) Compile(Query query)
     {
         StringBuilder message = new StringBuilder();
         StringBuilder paramsBinding = new();
+        message.Append("SQL String: ");
         paramsBinding.Append("Bindings: ");
         message.Append("SELECT ");
         foreach (var column in query._columns)
         {
-            message.Append('"').Append(column).Append('"').Append(", ");
+            message.Append('[').Append(column).Append(']').Append(", ");
         }
         if (message.Length >= 2)
         {
             message.Remove(message.Length - 2, 2);
         }
         message.Append(' ');
-        message.Append("FROM ").Append('"').Append(query._tableName).Append('"').Append(' ');
+        message.Append("FROM ").Append('[').Append(query._tableName).Append(']').Append(' ');
         message.Append("WHERE ");
-        int index = 1;
+        int index = 0;
         paramsBinding.Append('[');
         foreach (var conditional_key in query._columnName_value)
         {
-            message.Append('"').Append(conditional_key.Key).Append('"').Append(" = $").Append(index).Append(" AND ");
+            message.Append('[').Append(conditional_key.Key).Append(']').Append(" = @p").Append(index).Append(" AND ");
             paramsBinding.Append(conditional_key.Value).Append(", ");
             index++;
         }
