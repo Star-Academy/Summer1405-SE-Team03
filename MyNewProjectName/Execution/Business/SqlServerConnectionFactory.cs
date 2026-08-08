37291226f40912a932b1af0b11ceb0b1efbd58d2
Mahdi Kazemaini <mokazemaini84@gmail.com>
2026-08-08 14:59:45 +0330

    refactoring ctor

--- MyNewProjectName/Execution/Business/SqlServerConnectionFactory.cs
+++ MyNewProjectName/Execution/Business/SqlServerConnectionFactory.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Data;
 using Microsoft.Data.SqlClient;
 using MyNewProjectName.Execution.Abstractions;
 
 namespace MyNewProjectName.Execution.Business;
 
-internal sealed class SqlServerConnectionFactory(string connectionString) : IDbConnectionFactory
+internal sealed class SqlServerConnectionFactory : IDbConnectionFactory
 {
-    public IDbConnection CreateConnection() => new SqlConnection(connectionString);
+    private readonly string _connectionString;
+
+    public SqlServerConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+    }
+
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
 }
