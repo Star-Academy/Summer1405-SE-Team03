d8054735c439880e28ae190791a59de1ddeedf50
Mahdi Kazemaini <mokazemaini84@gmail.com>
2026-08-08 17:06:26 +0330

    test for SqlServerSyntaxFormatter

---
+++ MyNewProjectName.Tests/Grammars/SqlServerSyntaxFormatterTests.cs
@@ -0,0 +1,34 @@
+﻿using MyNewProjectName.Grammars.Business;
+using FluentAssertions;
+using Xunit;
+
+namespace MyNewProjectName.Tests.Grammars;
+public class SqlServerSyntaxFormatterTests
+{
+    [Fact]
+    public void FormatIdentifier_Should_WrapStringInDoubleQuotes_When_CalledWithValidString()
+    {
+        var sqlServerSyntaxFormatter = new SqlServerSyntaxFormatter();
+        var columnName = "studentnumber";
+        var sqlServerResultFormatter =  sqlServerSyntaxFormatter.FormatIdentifier(columnName);
+        sqlServerResultFormatter.Should().Be("[studentnumber]");
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void GetParameterName_Should_ReturnParameterWithAtPrefixedAtP_When_IndexIsProvided(int index)
+    {
+        var sqlServerSyntaxFormatter = new SqlServerSyntaxFormatter();
+        var sqlServersResultFormatter = sqlServerSyntaxFormatter.GetParameterName(index);
+        sqlServersResultFormatter.Should().Be($"@p{index}");
+    }
+
+    [Fact]
+    public void ParametrStartIndex_Should_BeZero_When_YouWantToAccessTheDatabase()
+    {
+        var sqlServerSyntaxFormatter = new SqlServerSyntaxFormatter();
+        var sqlServerResultFormatter = sqlServerSyntaxFormatter.ParameterStartIndex;
+        sqlServerResultFormatter.Should().Be(0);
+    }
+}
