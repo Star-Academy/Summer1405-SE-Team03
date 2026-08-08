9945a360616335f3b9b49d82789bfd554ec1c6df
amirabbasghg <amirabbasghg@gmail.com>
2026-08-08 16:42:45 +0330

    StudentQueryResultPresenterTests

---
+++ MyNewProjectName.Tests/Presentation/StudentQueryResultPresenterTests.cs
@@ -0,0 +1,32 @@
+using System.Data;
+using MyNewProjectName.Presentation.Business;
+
+namespace MyNewProjectName.Tests.Presentation;
+
+public class StudentQueryResultPresenterTests
+{
+    [Fact]
+    public void StudentQueryResultPresenter_Constructor()
+    {
+        var table = new DataTable();
+        table.Columns.Add("studentnumber", typeof(string));
+        table.Columns.Add("firstname", typeof(string));
+        table.Rows.Add("123", "Ali");
+        table.Rows.Add("456", "Reza");
+
+        using var reader = table.CreateDataReader();
+        var presenter = new StudentQueryResultPresenter();
+
+        using var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        // Act
+        presenter.PresentResults(reader);
+
+        // Assert
+        var output = stringWriter.ToString();
+        Assert.Contains("Student Number: 123, Name: Ali", output);
+        Assert.Contains("Student Number: 456, Name: Reza", output);
+        
+}
+    }
