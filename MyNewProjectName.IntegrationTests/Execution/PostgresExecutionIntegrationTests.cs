0c36cad57d12b998cfccd9c82e05a5a06e6a8848
Mahdi Kazemaini <mokazemaini84@gmail.com>
2026-08-10 10:32:42 +0330

    add dir and files

---
+++ MyNewProjectName.IntegrationTests/Execution/PostgresExecutionIntegrationTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using MyNewProjectName.Compilers.Business;
+using MyNewProjectName.Core;
+using MyNewProjectName.Execution.Business;
+using MyNewProjectName.Grammars.Business;
+using MyNewProjectName.IntegrationTests.Fixtures;
+using MyNewProjectName.Presentation.Abstractions;
+using Npgsql;
+using NSubstitute;
+using Xunit;
+namespace MyNewProjectName.IntegrationTests.Execution;
+
+public class PostgresExecutionIntegrationTests
+{
+    
+}
---
+++ MyNewProjectName.IntegrationTests/Execution/SqlServerExecutionIntegrationTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using MyNewProjectName.Compilers.Business;
+using MyNewProjectName.Core;
+using MyNewProjectName.Execution.Business;
+using MyNewProjectName.Grammars.Business;
+using MyNewProjectName.IntegrationTests.Fixtures;
+using MyNewProjectName.Presentation.Abstractions;
+using NSubstitute;
+using Xunit;
+namespace MyNewProjectName.IntegrationTests.Execution;
+
+public class SqlServerExecutionIntegrationTests
+{
+    
+}
