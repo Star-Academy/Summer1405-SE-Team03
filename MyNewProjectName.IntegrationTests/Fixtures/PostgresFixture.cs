cb34855a9999c2d455ef62b645dcc5e9506f5a60
Mahdi Kazemaini <mokazemaini84@gmail.com>
2026-08-10 10:30:34 +0330

    test(integration): add Postgres and SqlServer database fixtures using Testcontainers

---
+++ MyNewProjectName.IntegrationTests/Fixtures/PostgresFixture.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace MyNewProjectName.IntegrationTests.Fixtures;
+
+public sealed class PostgresDatabaseFixture : IAsyncLifetime
+{
+    private PostgreSqlContainer Container { get; set; } = null!;
+    public string ConnectionString => Container.GetConnectionString();
+
+    public async Task InitializeAsync()
+    {
+        Container = new PostgreSqlBuilder("postgres:16-alpine")
+            .WithDatabase("psgDB")
+            .WithUsername("psg")
+            .WithPassword("psg")
+            .Build();
+
+        await Container.StartAsync();
+    }
+    
+    public async Task DisposeAsync()
+    {
+        await Container.DisposeAsync();
+    }
+}
---
+++ MyNewProjectName.IntegrationTests/Fixtures/SqlServerDatabaseFixture.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Testcontainers.MsSql;
+using Xunit;
+
+namespace MyNewProjectName.IntegrationTests.Fixtures;
+
+public sealed class SqlServerDatabaseFixture : IAsyncLifetime
+{
+    private MsSqlContainer Container { get; set; } = null!;
+    public string ConnectionString => Container.GetConnectionString();
+
+    public async Task InitializeAsync()
+    {
+        Container = new MsSqlBuilder("mcr.microsoft.com/mssql/server:2022-latest")
+            .WithPassword("Secret123!")
+            .Build();
+
+        await Container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Container.DisposeAsync();
+    }
+}
