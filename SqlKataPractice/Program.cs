b16cda94d94355b46dc924df7b8a0c959b73c067
amirabbasghg <amirabbasghg@gmail.com>
2026-08-10 18:04:33 +0330

    complete setup and compiler comparison checklist

--- SqlKataPractice/Program.cs
+++ SqlKataPractice/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SqlKata;
+using SqlKata.Compilers;
+
+var query = new Query("Students")
+    .Select("studentnumber", "firstname")
+    .Where("ismale", false)
+    .Where("grade", ">=", 19.24)
+    .Where("firstname", "Reza");
+
+var postgresCompiler = new PostgresCompiler();
+var postgresResult = postgresCompiler.Compile(query);
+
+var sqlServerCompiler = new SqlServerCompiler();
+var sqlServerResult = sqlServerCompiler.Compile(query);
+
+Console.WriteLine(postgresResult);
+Console.WriteLine(sqlServerResult);
+
+Console.WriteLine("SQL:");
+Console.WriteLine(postgresResult.Sql);
+
+
+Console.WriteLine("Bindings:");
+for (int i = 0; i < postgresResult.Bindings.Count; i++)
+{
+    var value = postgresResult.Bindings[i];
+
+    Console.WriteLine($"@p{i} = {value}");
+}
+
+Console.WriteLine("SQL:");
+Console.WriteLine(sqlServerResult.Sql);
+Console.WriteLine("Bindings:");
+for (int i = 0; i < sqlServerResult.Bindings.Count; i++)
+{
+    var value = sqlServerResult.Bindings[i];
+
+    Console.WriteLine($"@p{i} = {value}");
+}
